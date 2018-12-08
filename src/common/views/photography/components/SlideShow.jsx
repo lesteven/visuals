abe4f3ae128d408f95616c50ab0d07d0f5e38f85
lesteven <le_steven@outlook.com>
2018-12-07 18:19:46 -0800

    add click scroller

--- src/common/views/photography/components/SlideShow.jsx
+++ src/common/views/photography/components/SlideShow.jsx
@@ -2,14 +2,31 @@ import React, { Component, Fragment } from 'react';
 import styles from '../css/slideshow.css';
 
 class SlideShow extends Component {
+  left = () => {
+    const div = document.getElementsByClassName('slideshow')[0];
+    div.scrollBy(-500, 0);
+  }
+  right = () => {
+    const div = document.getElementsByClassName('slideshow')[0];
+    div.scrollBy(500, 0);
+  }
   render() {
   const { data } = this.props;
     return (
-      <section className = 'slideshow'>
-        { data.map(each =>
-          <div key = { each } className = 'slide-image'>
-            <img src = { each } />
-          </div> )}
+      <section className = 'slideshow-wrapper'>
+        <div className = 'slide-left' onClick = { this.left }>
+          <div className = 'left-arrow'></div>
+        </div>
+        <section className = 'slideshow'>
+          { data.map((each,index) =>
+            <div key = { each } className = 'slide-image' 
+              id = { index }>
+              <img src = { each } />
+            </div> )}
+        </section>
+        <div className = 'slide-right' onClick = { this.right }>
+          <div className = 'right-arrow'></div>
+        </div>
       </section>
     )
   }
